299bc0c09bc196c3e26ca89983d55020b28ff345
(Noah) Hanlun Song <hsong@air.org>
2019-10-23 16:56:59 -0400

    Add types

---
+++ src/bin/types.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // scalar types
+    println!("scalar types: integer, floating-point, boolean, character");
+    println!("compound types: tuple");
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+    println!(
+        "coming from a tuple: {}, {}, {}, {}, {}, {}",
+        x, y, z, five_hundred, six_point_four, one
+    );
+    let a = [3; 5];
+    let b = [3, 3, 3, 3, 3];
+    println!("two ways of writing the same array is {}", a == b);
+}
