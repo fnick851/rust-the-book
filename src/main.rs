a0741f5fe7f62148f7fddac7eed4cf55c4e96800
(Noah) Hanlun Song <hsong@air.org>
2019-10-23 15:13:40 -0400

    Add `bin` folder

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
-
 fn main() {
-    let stdout = stdout();
-    let out = b"Hello, World!";
-    let width = 13;
-    let mut writer = BufWriter::new(stdout.lock());
-    say(out, width, &mut writer).unwrap();
+    println!("hello world");
 }
