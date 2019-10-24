18b15456e66bc189e63d99c935321bc0c6db3355
Noah <hanlun.song@gmail.com>
2019-10-24 16:21:42 -0400

    Add module and super

---
+++ src/bin/restrt_lib.rs
@@ -0,0 +1,29 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {
+            println!("added to wl");
+        }
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+fn main() {
+    pub fn eat_at_restaurant() {
+        // Absolute path
+        crate::front_of_house::hosting::add_to_waitlist();
+
+        // Relative path
+        front_of_house::hosting::add_to_waitlist();
+    }
+
+    eat_at_restaurant();
+}
